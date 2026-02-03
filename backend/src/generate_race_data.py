5b9c0d28f6918f8c33c0bf4cc5072de366acbb58
Jack L <jack.le.031231@hotmail.com>
2026-02-02 17:02:22 -0700

    modularizing

--- backend/src/generate_race_data.py
+++ backend/src/generate_race_data.py
@@ -7,11 +7,25 @@ import numpy as np
 from race_types import DriverTelemetry, RaceFrame, RaceData
 from typing import List, Dict, Union, TypedDict, Any
 
+fastf1.Cache.enable_cache('../cache') 
+
+def load_session(year: int, location: str, session_type: str):
+    session = fastf1.get_session(year, location, session_type)
+    session.load()
+    return session
+
+def build_track_map(session: fastf1.Session):
+    map_lap = session.laps.pick_fastest()
+    #TO DO WE ARE SEPERATING GENERATE RACE DATA INTO SMALLER FUNCTIONS
+    #must build map here
+
 
 def generate_race_data(year: int, location: str):
 
     print("Constructing timeline...")
-    all_indices = pd.Index([])
+    session = load_session(year, location, 'R')
+    #call build track map here
+
 
 
 
